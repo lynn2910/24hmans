27bd8e560311105b5cb0f0f0a503b6971764d240
lynn2910 <cedric.colin35@gmail.com>
2024-09-16 09:34:32 +0200

    Add code quality check

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-require("dotenv").config()
+require("dotenv").config();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 app.listen(process.env.API_PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://127.0.0.1:${process.env.API_PORT}`)
-})
+  console.log(`Server running on http://127.0.0.1:${process.env.API_PORT}`);
+});
