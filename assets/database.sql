da25ec41676ee8cfc4d30feba65835f31f51c3eb
--get <cedic2005@gmail.com>
2024-10-17 16:24:48 +0200

    Base de donnée de la boutique

--- assets/database.sql
+++ assets/database.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS shop_items;
+DROP TABLE IF EXISTS shop_items_category;
+DROP TABLE IF EXISTS shop;
+
 DROP TABLE IF EXISTS prestataires;
 
 CREATE TABLE prestataires
@@ -10,6 +14,57 @@ CREATE TABLE prestataires
     PRIMARY KEY (prestataire_id)
 );
 
+
+#
+#
+# BOUTIQUE
+#
+#
+
+CREATE TABLE shop
+(
+    shop_id        CHAR(36) NOT NULL,
+    prestataire_id CHAR(36) NOT NULL,
+
+    FOREIGN KEY (prestataire_id) REFERENCES prestataires (prestataire_id),
+    PRIMARY KEY (shop_id)
+);
+
+CREATE TABLE shop_items_category
+(
+    shop_id        CHAR(36)    NOT NULL,
+    category_id    CHAR(36)    NOT NULL,
+
+    category_label VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE shop_items
+(
+    id_shop     CHAR(36)           NOT NULL,
+    item_id     INT AUTO_INCREMENT NOT NULL,
+
+    name        VARCHAR(128)       NOT NULL,
+    image       VARBINARY(256),
+
+    category    CHAR(36),
+
+    stock       INT                NOT NULL DEFAULT 0,
+    price       DECIMAL(6, 2)      NOT NULL,
+    description VARCHAR(4096)      NOT NULL DEFAULT '',
+
+    FOREIGN KEY (category) REFERENCES shop_items_category (category_id),
+    PRIMARY KEY (item_id, id_shop)
+);
+
+#
+#
+#   JEU DE TESTS
+#
+#
+
+
 INSERT INTO prestataires (name, prestataire_id, icon, password)
 VALUES ('Porsche', '45309281-fc24-4e02-ad47-a275c64f5327', 'prestataires_icons/porsche_presta.jpg',
         'a8583ccd0f2fe6d789fda0b3ff80711c8141b543b0334f3888fc11b52914a90e'),
@@ -19,3 +74,18 @@ VALUES ('Porsche', '45309281-fc24-4e02-ad47-a275c64f5327', 'prestataires_icons/p
         'da85e329212776cba7df7e11b396db625f4e20d8b747f99e0ccb0781b14c052e'),
        ('Mong\'man', '255da203-781d-4e50-924f-0423638cdb68', 'prestataires_icons/montgol_presta.jpg',
         '32de3639ca6fafb56a4b3c68f42cfe8a686c89d92b173a03becdcc02644d7511');
+
+INSERT INTO shop (shop_id, prestataire_id)
+    VALUE ('867fb638-7cb1-4228-a643-5c4f352f44b1', '45309281-fc24-4e02-ad47-a275c64f5327');
+
+INSERT INTO shop_items_category (shop_id, category_id, category_label)
+VALUES ('867fb638-7cb1-4228-a643-5c4f352f44b1', 'be2cff03-7d12-4369-acff-037d12a36993', 'Porte-clé'),
+       ('867fb638-7cb1-4228-a643-5c4f352f44b1', '9af710a9-9c13-43d7-b710-a99c1323d77d', 'Écusson');
+
+INSERT INTO shop_items (id_shop, item_id, name, category, stock, price)
+VALUES ('867fb638-7cb1-4228-a643-5c4f352f44b1', 1, 'Porte-clé frein', 'be2cff03-7d12-4369-acff-037d12a36993', 79,
+        16.99),
+       ('867fb638-7cb1-4228-a643-5c4f352f44b1', 2, 'Porte-clé porsche', 'be2cff03-7d12-4369-acff-037d12a36993', 146,
+        24.99),
+       ('867fb638-7cb1-4228-a643-5c4f352f44b1', 3, 'Porte-clé frein', '9af710a9-9c13-43d7-b710-a99c1323d77d', 14,
+        16.99);
