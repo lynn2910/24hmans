d95b54a95e812057881aa093c4b3692c6a20f6d1
--get <cedic2005@gmail.com>
2024-10-21 15:54:54 +0200

    Ajout de la directive 'v-click-outside'

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,20 @@ import './index.css'
 
 Vue.config.productionTip = false
 
+Vue.directive('click-outside', {
+    bind: function (el, binding, node) {
+        el.clickOutsideEvent = function (event) {
+            if (!(el === event.target || el.contains(event.target))) {
+                node.context[binding.expression](event);
+            }
+        };
+        document.body.addEventListener('click', el.clickOutsideEvent)
+    },
+    unbind: function (el) {
+        document.body.removeEventListener('click', el.clickOutsideEvent)
+    },
+});
+
 new Vue({
     router,
     store,
