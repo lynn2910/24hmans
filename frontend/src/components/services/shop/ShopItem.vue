8cdfc2668a3bf70637cc4ac79fe4fd51099a1646
--get <cedic2005@gmail.com>
2024-11-12 14:35:39 +0100

    feat: Add stock indicators in the shop page

--- frontend/src/components/services/shop/ShopItem.vue
+++ frontend/src/components/services/shop/ShopItem.vue
@@ -8,9 +8,13 @@
 		<img v-else class="h-20 w-20 mx-auto text-center opacity-90 mb-2" src="@/assets/images/no_image.png"
 				 alt="Aucune image">
 		<h3 class="mx-auto text-xl text-center font-bold italic group-hover:text-blue-500">{{ item.name }}</h3>
+
 		<p class="mx-auto mt-5 text-xl font-semibold text-center ml-4">
 			{{ item.price.toFixed(2) }} €</p>
-		<ShopitemCategory class="ml-2 mt-7" :category="category"></ShopitemCategory>
+		<!-- Indicateur de stock -->
+		<p :class="isStockVisible + ' ' + stockColor" class="text-center mt-0">{{ item.stock }} produits restants</p>
+
+		<ShopItemCategory class="ml-2 mt-7" :category="category"></ShopItemCategory>
 	</router-link>
 </template>
 
@@ -27,14 +31,26 @@
             -->
 
 <script>
-import ShopitemCategory from "@/components/services/shop/ShopItemCategory.vue";
+import ShopItemCategory from "@/components/services/shop/ShopItemCategory.vue";
+import ItemBigView from "@/components/services/shop/ItemBigView.vue";
 
 export default {
 	name: 'ShopItem',
-	components: {ShopitemCategory},
+	components: {ShopItemCategory},
 	props: {
 		item: Object,
 		category: String,
+	},
+	computed: {
+		ItemBigView() {
+			return ItemBigView
+		},
+		isStockVisible() {
+			return this.item.stock > 30 ? 'opacity-0' : 'visible'
+		},
+		stockColor() {
+			return ItemBigView.methods.getStockColor(this.item.stock)
+		}
 	}
 }
 </script>
