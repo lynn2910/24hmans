3342b42d7a9882583de8c4338c609cd5cd7cbba6
sugdenaz <sugdenaz.ekici@edu.univ-fcomte.fr>
2024-12-03 10:23:34 +0100

    ajout fichier presta dashboard ecurie

---
+++ frontend/src/views/panels/prestataire/PrestataireDashboardVisiteEcuries.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PrestataireDashboardTemplate from "@/components/dashboard/prestataire/PrestataireDashboardTemplate.vue";
+</script>
+
+<template>
+    <PrestataireDashboardTemplate current-page="home">
+        <p>visite écuries</p>
+    </PrestataireDashboardTemplate>
+</template>
