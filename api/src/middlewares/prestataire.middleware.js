32ac148e7dd9b370ecd58e2f69fe2637ea8bc3f5
lynn2910 <cedic2005@gmail.com>
2024-12-21 14:46:44 +0100

    feat(api): Change authentification middleware to check 'session' query key (with no right check)

--- api/src/middlewares/prestataire.middleware.js
+++ api/src/middlewares/prestataire.middleware.js
@@ -1,10 +1,11 @@
 module.exports = function (req, res, next) {
-    // TODO ajouter le système de login (pour le moment c'est toujours true si le header "Authorization" contient "last_christmas_i_gave_you_my_heart")
+    const sessionId = req.query.session;
 
-    if ((req.headers.authorization || "").replace(/^Bearer /, '') !== "last_christmas_i_gave_you_my_heart") {
-        return res.status(401).json({message: "You are not authorized"});
+    if (!sessionId) {
+        return res.status(401).json({message: "Access denied"});
     }
+
+    // TODO vérifier les droits
 
     next();
 }
-// https://openclassrooms.com/en/courses/5614116-go-full-stack-with-node-js-express-and-mongodb/5656301-set-up-authentication-middleware
