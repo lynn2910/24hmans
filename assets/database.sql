03684db7c59157c4a5bd427ba6fd45f28f6c5b14
lynn2910 <cedic2005@gmail.com>
2024-12-25 11:20:50 +0100

    fix(database.sql): Correct SQL table names in the inserts

--- assets/database.sql
+++ assets/database.sql
@@ -5,7 +5,7 @@
 #
 
 
-INSERT INTO prestataire (name, id, icon, password, email, referencer)
+INSERT INTO Prestataire (name, id, icon, password, email, referencer)
 VALUES ('Porsche', '45309281-fc24-4e02-ad47-a275c64f5327', 'prestataires_icons/porsche_presta.jpg',
         '$2a$10$Jfw2HzNUl/FQk.30LkQJhOa1W3rr1RDT7KUOmTVbN.ucf44w3PlEK', 'porsche@gmail.com',
         'porsche'),                                                                                        -- "Spider-Porsche"
@@ -17,7 +17,7 @@ VALUES ('Porsche', '45309281-fc24-4e02-ad47-a275c64f5327', 'prestataires_icons/p
         '$2y$10$GhHncEKGwYMsbC72aLNae..OUpuvCn.a7Tvyq8VkCWn7r5UNXgRim', '24hmans@gmail.com', 'mongman');
 -- montgolfiere
 
-INSERT INTO prestatairelink (prestataire_id, name, url)
+INSERT INTO PrestataireLink (prestataire_id, name, url)
 VALUES ('45309281-fc24-4e02-ad47-a275c64f5327', 'Site officiel', 'https://www.porsche.com'),
        ('255da203-781d-4e50-924f-0423638cdb68', 'Nos prestations',
         'https://montgolfiere-france.com/bapteme-en-montgolfiere-pres-du-mans/'),
@@ -25,7 +25,7 @@ VALUES ('45309281-fc24-4e02-ad47-a275c64f5327', 'Site officiel', 'https://www.po
         'https://www.lemans-karting.com/karting/sessions-kart'),
        ('af3a0f62-5b13-4b19-9d42-736870b268a0', 'Notre site', 'https://24h.chamallow.fr');
 
-INSERT INTO user (id, email, password, first_name, last_name)
+INSERT INTO User (id, email, password, first_name, last_name)
 VALUES ('e052f135-13db-4a0d-aa15-f9bffac00359 ', 'test@gmail.com',
         '$2y$10$UwdnOZGp863rcdvYvpkiKOB5Cc3DCt3LtYHvIILE4eWqOGINTgiQO', 'Test', 'Family'),
        ('f4f434b3-f256-484b-8935-29e13126c9e8', 'carla@gmail.com',
@@ -39,14 +39,14 @@ VALUES ('e052f135-13db-4a0d-aa15-f9bffac00359 ', 'test@gmail.com',
 # #       BOUTIQUE
 # #
 #
-INSERT INTO boutique (shop_id, prestataire_id, enabled)
+INSERT INTO Boutique (shop_id, prestataire_id, enabled)
     VALUE ('867fb638-7cb1-4228-a643-5c4f352f44b1', '45309281-fc24-4e02-ad47-a275c64f5327', true);
 
-INSERT INTO boutiquecategory (shop_id, category_id, category_label)
+INSERT INTO BoutiqueCategory (shop_id, category_id, category_label)
 VALUES ('867fb638-7cb1-4228-a643-5c4f352f44b1', 'be2cff03-7d12-4369-acff-037d12a36993', 'Porte-clé'),
        ('867fb638-7cb1-4228-a643-5c4f352f44b1', '9af710a9-9c13-43d7-b710-a99c1323d77d', 'Écusson');
 
-INSERT INTO boutiquearticles (shop_id, name, category_id, stock, price, referencer)
+INSERT INTO BoutiqueArticles (shop_id, name, category_id, stock, price, referencer)
 VALUES ('867fb638-7cb1-4228-a643-5c4f352f44b1', 'Porte-clé frein',
         'be2cff03-7d12-4369-acff-037d12a36993', 79,
         16.99, 'porte-clé-frein'),
