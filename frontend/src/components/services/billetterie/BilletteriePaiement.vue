7ec1cf221d6c86d292b7fc9889d4e4dc4dead7ad
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2025-01-06 14:14:39 +0100

    paiement billetterie vue

--- frontend/src/components/services/billetterie/BilletteriePaiement.vue
+++ frontend/src/components/services/billetterie/BilletteriePaiement.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1 class="text-2xl font-bold my-3">Paiement</h1>
+    <div>
+        <h1 class="text-2xl font-bold my-3">Paiement</h1>
+        <div>
+            <p>Service non disponible pour le moment</p>
+        </div>
+    </div>
 </template>
 
 <script>
