c89bc7eea917944b9312812603ba41acd670c5db
lynn2910 <cedic2005@gmail.com>
2025-01-09 11:22:20 +0100

    feat(api): Add swagger documentation for GET /users/@me route

--- api/src/routes/user.router.js
+++ api/src/routes/user.router.js
@@ -7,6 +7,44 @@ const {getUser} = require("../services/user.service");
 const {getPrestataire} = require("../services/prestataire.service");
 const {getAdmin} = require("../services/admin.service");
 
+/**
+ * @swagger
+ * /users/@me:
+ *   get:
+ *     summary: Get Current User Information
+ *     tags:
+ *       - User
+ *     description: Retrieves information about the currently authenticated user.
+ *     parameters:
+ *       - in: query
+ *         name: sessionId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The session ID of the authenticated user.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User information retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                 - $ref: '#/components/schemas/User'
+ *                 - $ref: '#/components/schemas/Prestataire'
+ *                 - $ref: '#/components/schemas/Admin'
+ *       500:
+ *         description: An error occurred while fetching user information.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ */
 router.get('/@me', userMiddleware, async (req, res) => {
     let user = req.session;
 
