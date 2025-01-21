56383e09c0508a4d623a4be7ae18081b6acbdf20
lynn2910 <cedic2005@gmail.com>
2025-01-21 15:18:23 +0100

    feat(api): add karting circuit POST and GET endpoints

--- api/src/routes/karting.router.js
+++ api/src/routes/karting.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const prestataireMiddleware = require('../middlewares/prestataire.middleware');
 const KartingService = require("../services/karting.service");
+const {createRule, Method, Permission, User} = require("../permissions");
 
 /**
  * @swagger
@@ -116,9 +117,171 @@ router.get("/:karting_id/", async (req, res) => {
     }
 })
 
-// Circuit
-router.get("/:karting_id/circuit")
-router.post("/:karting_id/circuit")
+//
+//
+//       Circuit
+//
+//
+
+/**
+ * @swagger
+ * /karting/{karting_id}/circuit:
+ *   post:
+ *     summary: "Create Circuit for Karting"
+ *     tags:
+ *       - Karting
+ *     description: "Creates a new circuit for a specific karting."
+ *     parameters:
+ *       - in: query
+ *         name: sessionId
+ *         example: "sdkhd4Kcr8"
+ *         schema:
+ *           type: string
+ *         description: The session ID of the authenticated prestataire.
+ *       - in: path
+ *         name: karting_id
+ *         required: true
+ *         example: "29e11c0e-ee8f-4ab2-91a0-31486c5aeb19"
+ *         schema:
+ *           type: string
+ *         description: The ID of the karting.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               {
+ *                 "minAge": 18,
+ *                 "circuit_name": "Adultes",
+ *                 "kart_power": 270
+ *               }
+ *             properties:
+ *               minAge:
+ *                 type: integer
+ *                 description: The minimum age requirement for the circuit.
+ *               circuit_name:
+ *                 type: string
+ *                 description: The name of the circuit.
+ *               kart_power:
+ *                 type: string
+ *                 description: The power of the karts used on the circuit.
+ *     responses:
+ *       200:
+ *         description: Circuit created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Circuit'
+ *       400:
+ *         description: Invalid request body.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ *       500:
+ *         description: An error occurred while creating the circuit.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ */
+router.post("/:karting_id/circuit", prestataireMiddleware, async (req, res) => {
+    let fields = ['minAge', 'circuit_name', 'kart_power'];
+    if (typeof req.body !== "object" || Array.isArray(req.body) || fields.some(k => !(k in req.body))) {
+        res.status(400).json({message: "Invalid body field"});
+        return;
+    }
+
+    KartingService.create_circuit(req.params.karting_id, req.session.userId, req.body).then(
+        (circuit) => res.status(200).json(circuit),
+        ({status, message}) => res.status(status).json({message})
+    )
+})
+createRule("/karting/:karting_id/circuit", Method.All, User.Prestataire, [Permission.Prestataire]);
+
+/**
+ * @swagger
+ * /karting/{karting_id}/circuit/{circuit_id}:
+ *   get:
+ *     summary: Get Circuit Details
+ *     tags:
+ *       - Karting
+ *     description: Retrieves detailed information about a specific circuit for a given karting.
+ *     parameters:
+ *       - in: path
+ *         name: karting_id
+ *         example: "29e11c0e-ee8f-4ab2-91a0-31486c5aeb19"
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the karting.
+ *       - in: path
+ *         name: circuit_id
+ *         required: true
+ *         example: "4db3c4e0-0504-4ac4-902a-dfc98d6455de"
+ *         schema:
+ *           type: string
+ *         description: The ID of the circuit.
+ *       - in: query
+ *         name: sessionId
+ *         example: "sdkhd4Kcr8"
+ *         schema:
+ *           type: string
+ *         description: The session ID of the authenticated prestataire.
+ *     responses:
+ *       200:
+ *         description: Circuit details retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Circuit'
+ *       404:
+ *         description: Circuit not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ *       500:
+ *         description: An error occurred while fetching circuit details.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ */
+router.get("/:karting_id/circuit/:circuit_id", async (req, res) => {
+    if (req.query.sessionId) {
+        await prestataireMiddleware(req, res, async () => {
+            await KartingService.get_karting_circuit(req.params.karting_id, req.params.circuit_id, req.session.userId).then(
+                (circuit) => res.status(200).json(circuit),
+                ({status, message}) => res.status(status).json({message})
+            );
+        })
+    } else {
+        await KartingService.get_karting_circuit(req.params.karting_id, req.params.circuit_id).then(
+            (circuit) => res.status(200).json(circuit),
+            ({status, message}) => res.status(status).json({message})
+        );
+    }
+})
+
 router.patch("/:karting_id/circuit/:circuit_id")
 router.delete("/:karting_id/circuit/:circuit_id")
 
--- api/src/services/karting.service.js
+++ api/src/services/karting.service.js
@@ -1,4 +1,5 @@
-const prisma = require("../db")
+const prisma = require("../db");
+const uuid = require("uuid");
 
 function get_available_kartings(prestataire_id = null) {
     if (prestataire_id) {
@@ -69,7 +70,82 @@ function get_karting(karting_id, prestataire_id = null) {
     })
 }
 
+//
+//
+//          CIRCUIT
+//
+//
+
+function create_circuit(karting_id, prestataire_id, {minAge, circuit_name, kart_power}) {
+    return new Promise(async (resolve, reject) => {
+        // Create karting if it doesn't exist
+        let id = uuid.v4();
+
+        try {
+            let circuit_id = uuid.v4().toString();
+
+            let circuit = await prisma.kartingCircuit.create({
+                data: {
+                    circuit_id, minAge, circuit_name, kart_power,
+                    karting: {
+                        connectOrCreate: {
+                            create: {
+                                karting_id: id,
+                                prestataire: {
+                                    connect: {
+                                        id: prestataire_id,
+                                    }
+                                },
+                                reservations: {
+                                    create: {
+                                        app_id: uuid.v4()
+                                    }
+                                }
+                            },
+                            where: {
+                                karting_id
+                            }
+                        }
+                    }
+                },
+            })
+
+            return resolve(circuit);
+        } catch (err) {
+            console.error(err)
+            return reject({status: 500, message: err.message});
+        }
+    })
+}
+
+function get_karting_circuit(karting_id, circuit_id, prestataire_id = null) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let filter = prestataire_id ? {karting: {prestataire_id}} : {};
+
+            let circuit = await prisma.kartingCircuit.findFirst({
+                where: {
+                    karting_id, circuit_id,
+                    ...filter
+                }
+            });
+
+            return resolve(circuit);
+        } catch (err) {
+            if (err.code === 'P2025') {
+                return reject({status: 404, message: "Circuit not found"})
+            }
+
+            console.error(err)
+            return reject({status: 500, message: err.message});
+        }
+    })
+}
+
 module.exports = {
     get_available_kartings,
-    get_karting
+    get_karting,
+
+    create_circuit,
+    get_karting_circuit
 }
