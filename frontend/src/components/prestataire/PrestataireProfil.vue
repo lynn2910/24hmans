c1542943ac92eacff3e0c9fda328c60e861be93c
lynn2910 <cedric.colin35@gmail.com>
2025-03-31 16:12:49 +0200

    feat: Improve PrestataireProfil component and update translations

--- frontend/src/components/prestataire/PrestataireProfil.vue
+++ frontend/src/components/prestataire/PrestataireProfil.vue
@@ -39,7 +39,9 @@ export default {
 	<div class="w-full mt-36 bg-dark">
 		<img v-if="prestataire?.icon" :src="`${publicPath}${prestataire?.icon}`" alt="Photo de profil"
 				 class="mx-auto rounded-full w-44 h-44">
-		<h1 class="font-extrabold text-4xl text-center py-4">{{ prestataire?.name }}</h1>
+		<h1 class="font-extrabold text-4xl text-center py-4">
+			{{ prestataire?.name || prestataire.referencer }}
+		</h1>
 		<div class="flex flex-row mx-auto justify-center content-center">
 			<a v-for="(link, index) in (prestataire?.links || [])" :key="index" :href="link.url"
 				 class="py-2 px-5 bg-gray-700 mx-5 font-medium rounded-3xl hover:bg-gray-500 text-xl">
