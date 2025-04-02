1908f4a5f8bdb3c469464a733d07035fd2a6c686
lynn2910 <cedric.colin35@gmail.com>
2025-04-02 14:20:59 +0200

    feat(Shop): Remove old todo

--- frontend/src/views/panels/prestataire/shop/PrestataireShopArticles.vue
+++ frontend/src/views/panels/prestataire/shop/PrestataireShopArticles.vue
@@ -72,7 +72,6 @@
 										d="M19 19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .896-2 2v14c0 1.104.897 2 2 2h14a2 2 0 0 0 2-2v-8.668l-2 2V19z"></path>
 							</svg>
 						</div>
-						<!-- TODO modifier les informations d'un article -->
 						<!-- Delete -->
 						<div class="px-2 cursor-pointer fill-red-500 hover:fill-red-400 my-auto"
 								 @click="$emit('articleDelete', article.item_id)">
