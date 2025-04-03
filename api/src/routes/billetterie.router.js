1ad89bbdb8db21520d71556882f076e5ec4a679d
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2025-04-03 13:39:42 +0200

    création de la route post de la billetterie, fonctionne pas pour le moment

--- api/src/routes/billetterie.router.js
+++ api/src/routes/billetterie.router.js
@@ -2,6 +2,9 @@ const {Router} = require("express");
 const {getBilletterieFromName, getBilletterie} = require("../services/billetterie.service");
 
 const routerBilletterie = new Router();
+const {createRule, Method, User, Permission} = require("../permissions");
+const {authenticateToken} = require("../middlewares/auth.middleware");
+const {getUserOrders, createNewOrder} = require("../services/user.service");
 
 
 /**
@@ -98,5 +101,170 @@ routerBilletterie.get("/:prestataire_name", async (req, res) => {
         res.status(404).json({message: "billetterie not found"});
     }
 });
+
+/**
+ * @swagger
+ * /billetterie/{billetterie_name}/@me/orders:
+ *   post:
+ *     tags:
+ *       - Billetterie
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Create a new order in the billetterie
+ *     description: Creates a new order for the authenticated user in the specified billetterie.
+ *     parameters:
+ *       - in: path
+ *         name: billetterie_name
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The name of the billetterie.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               {
+ *                 "user_id": "6c0996c1-d692-4051-9cf5-25cc91e0eb7b",
+ *                 "category": {
+ *                   "category_label": "Tribune",
+ *                   "category_id": 1
+ *                 },
+ *                 "date": [
+ *                   {
+ *                     "forfait_label": "Jeudi 12 juin 2025",
+ *                     "forfait_id": 2
+ *                   }
+ *                 ],
+ *                 "nbPersonnes": [
+ *                   "5", "", "", "", ""
+ *                 ],
+ *                 "created_at": "2025-04-03T10:54:19.651Z"
+ *               }
+ *             properties:
+ *               user_id:
+ *                 type: string
+ *                 description: The ID of the user creating the order.
+ *               category:
+ *                 type: object
+ *                 properties:
+ *                   category_label:
+ *                     type: string
+ *                     description: The label of the category.
+ *                   category_id:
+ *                     type: integer
+ *                     description: The ID of the category.
+ *               date:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     forfait_label:
+ *                       type: string
+ *                       description: The label of the forfait.
+ *                     forfait_id:
+ *                       type: integer
+ *                       description: The ID of the forfait.
+ *               nbPersonnes:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: The number of persons included in the order.
+ *               created_at:
+ *                 type: string
+ *                 format: date-time
+ *                 description: The date and time when the order was created.
+ *     responses:
+ *       200:
+ *         description: Order created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               example:
+ *                 {
+ *                   "user_id": "6c0996c1-d692-4051-9cf5-25cc91e0eb7b",
+ *                   "category": {
+ *                     "category_label": "Tribune",
+ *                     "category_id": 1
+ *                   },
+ *                   "date": [
+ *                     {
+ *                       "forfait_label": "Jeudi 12 juin 2025",
+ *                       "forfait_id": 2
+ *                     }
+ *                   ],
+ *                   "nbPersonnes": [
+ *                     "5", "", "", "", ""
+ *                   ],
+ *                   "created_at": "2025-04-03T10:54:19.651Z"
+ *                 }
+ *       400:
+ *         description: Invalid request body.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: string
+ *                   description: The error code.
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ *       500:
+ *         description: An error occurred while creating the order.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The error message.
+ */
+
+routerBilletterie.post("/:billetterie_name/@me/orders", authenticateToken, async (req, res) => {
+    // On attend d'avoir ces infos dans le body:
+    //{
+    //  "user_id":"6c0996c1-d692-4051-9cf5-25cc91e0eb7b",
+    //  "category":
+    //      {
+    //          "category_label":"Tribune",
+    //          "category_id":1
+    //       },
+    //   "date":
+    //      [
+    //          {
+    //              "forfait_label":"Jeudi 12 juin 2025",
+    //              "forfait_id":2
+    //           }
+    //       ],
+    //   "nbPersonnes":["5","","","",""],
+    //}
+
+    if (req.user?.userType !== User.User) {
+        res.status(401).json({message: "You are not a user"});
+        return;
+    }
+
+    let raw_order = req.body;
+
+    // Check that the body corresponds to what we want
+    let is_body_invalid = ['user_id', 'category', 'date',"nbPersonnes","created_at"].some(k => !(k in raw_order))
+        || !Array.isArray(raw_order.articles)
+        || raw_order.articles.some((a) => !(('article_id' in a) && ('amount' in a)));
+
+    if (is_body_invalid) {
+        res.status(400).json({code: "INVALID_BODY", message: "The body doesn't have the proper required structure"});
+        return;
+    }
+
+    createNewOrder(req.user.id, raw_order)
+        .then((order) => res.status(200).json({order}))
+        .catch((err) => res.status(err.status).json({message: err.message}))
+})
 
 module.exports = routerBilletterie;
--- frontend/src/services/billetterie.service.js
+++ frontend/src/services/billetterie.service.js
@@ -4,21 +4,24 @@ import {Request} from "./axios.service"
 async function getBilletterieInformations(prestataire_name) {
     try {
         const res = await Request.get(`/billetterie/${prestataire_name}`);
-        console.log("🛠 Réponse brute API :", res);  // Vérifie toute la réponse Axios
 
         if (res && res.data) {
-            console.log("✅ Données API retournées :", res.data);
             return res.data;
         } else {
-            console.error("❌ Pas de données valides reçues.");
             return null;
         }
     } catch (error) {
-        console.error("❌ Erreur lors de la récupération des infos billetterie :", error);
         return null;
     }
 }
 
+async function newOrder(billetterie_name, order) {
+    return await Request.post("/billetterie/:billetterie_name/@me/orders")
+        .args({billetterie_name})
+        .body(order)
+        .send();
+}
+
 export default {
-    getBilletterieInformations,
+    getBilletterieInformations,newOrder
 }
