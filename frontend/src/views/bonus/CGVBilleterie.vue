dcd9bb9222fd30ccc5cfcf0bdda848fd73af3861
marvynlevin <gamerkyrhoz@gmail.com>
2025-04-05 09:54:07 +0200

    karting(udpate): languages errors + adapting style of pages

--- frontend/src/views/bonus/CGVBilleterie.vue
+++ frontend/src/views/bonus/CGVBilleterie.vue
@@ -1,22 +1,22 @@
 <template>
-	<div class="mt-36">
-		<div class="flex flex-col justify-center content-center items-center">
-			<h1 class="font-bold text-3xl text-center mb-10">{{ $t('bonuses.cgv.title') }}</h1>
+  <div class="mt-72">
+    <div class="flex flex-col justify-center content-center items-center">
+      <h1 class="font-bold text-3xl text-center mb-10">{{ $t('bonuses.cgv.title') }}</h1>
 
-			<div class="w-fit">
-				<p class="font-bold">{{ $t('bonuses.cgv.text') }}</p>
-				<a class="underline" target="_blank" href="/documents/CGV24hDuMans.pdf">CGV24hDuMans.pdf</a>
-			</div>
-		</div>
+      <div class="w-fit">
+        <p class="font-bold">{{ $t('bonuses.cgv.text') }}</p>
+        <a class="underline" target="_blank" href="/documents/CGV24hDuMans.pdf">CGV24hDuMans.pdf</a>
+      </div>
+    </div>
 
-		<FooterComponent></FooterComponent>
-	</div>
+    <FooterComponent></FooterComponent>
+  </div>
 </template>
 
 <script>
 import FooterComponent from "@/components/navigation/footer/FooterComponent.vue";
 
 export default {
-	components: {FooterComponent}
+  components: {FooterComponent}
 }
 </script>
--- frontend/src/views/bonus/MentionsLegales.vue
+++ frontend/src/views/bonus/MentionsLegales.vue
@@ -1,98 +1,98 @@
 <template>
-	<div class="mt-36">
-		<h1 class="font-bold text-3xl text-center mb-10">{{ $t('bonuses.mentions.title') }}</h1>
+  <div class="mt-36">
+    <h1 class="font-bold text-3xl text-center mb-10">{{ $t('bonuses.mentions.title') }}</h1>
 
-		<div class="mx-10 md:mx-40">
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.editor.title') }}</h2>
+    <div class="mx-10 md:mx-40">
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.editor.title') }}</h2>
 
-			<p>
-				{{ $t('bonuses.mentions.editor.text') }}
-				<br>
-				{{ $t('bonuses.mentions.editor.responsables') }}
-			</p>
-			<div class="grid grid-cols-2 grid-rows-5 w-40 mt-3 ml-5">
-				<p>{{ $t('bonuses.mentions.editor.cedric') }}</p>
-				<p>{{ $t('bonuses.mentions.editor.colin') }}</p>
+      <p>
+        {{ $t('bonuses.mentions.editor.text') }}
+        <br>
+        {{ $t('bonuses.mentions.editor.responsables') }}
+      </p>
+      <div class="grid grid-cols-2 grid-rows-5 w-40 mt-3 ml-5">
+        <p>Cédric</p>
+        <p>COLIN</p>
 
-				<p>Marvyn</p>
-				<p>LEVIN</p>
+        <p>Marvyn</p>
+        <p>LEVIN</p>
 
-				<p>Leslie</p>
-				<p>MERAT</p>
+        <p>Leslie</p>
+        <p>MERAT</p>
 
-				<p>Sugdenaz</p>
-				<p>EKICI</p>
+        <p>Sugdenaz</p>
+        <p>EKICI</p>
 
-				<p>Anna</p>
-				<p>GAIFFE</p>
-			</div>
+        <p>Anna</p>
+        <p>GAIFFE</p>
+      </div>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.host.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.host.title') }}</h2>
 
-			<p><strong>{{ $t('bonuses.mentions.host.name') }}</strong> Cédric COLIN</p>
-			<p><strong>{{ $t('bonuses.mentions.host.address') }}</strong></p>
-			<div class="ml-5">
-				<p>{{ $t('bonuses.mentions.host.address_line1') }}</p>
-				<p>{{ $t('bonuses.mentions.host.address_line2') }}</p>
-				<p>{{ $t('bonuses.mentions.host.address_line3') }}</p>
-			</div>
+      <p><strong>{{ $t('bonuses.mentions.host.name') }}</strong> Cédric COLIN</p>
+      <p><strong>{{ $t('bonuses.mentions.host.address') }}</strong></p>
+      <div class="ml-5">
+        <p>{{ $t('bonuses.mentions.host.address_line1') }}</p>
+        <p>{{ $t('bonuses.mentions.host.address_line2') }}</p>
+        <p>{{ $t('bonuses.mentions.host.address_line3') }}</p>
+      </div>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.intellectual.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.intellectual.title') }}</h2>
 
-			<p>{{ $t('bonuses.mentions.intellectual.text1') }}</p>
-			<p>{{ $t('bonuses.mentions.intellectual.text2') }}</p>
-			<p>{{ $t('bonuses.mentions.intellectual.text3') }}</p>
-			<p>{{ $t('bonuses.mentions.intellectual.text4') }}</p>
+      <p>{{ $t('bonuses.mentions.intellectual.text1') }}</p>
+      <p>{{ $t('bonuses.mentions.intellectual.text2') }}</p>
+      <p>{{ $t('bonuses.mentions.intellectual.text3') }}</p>
+      <p>{{ $t('bonuses.mentions.intellectual.text4') }}</p>
 
-			<br><br>
+      <br><br>
 
-			<p>{{ $t('bonuses.mentions.intellectual.contact') }}
-				<a class="underline text-blue-400"
-					 href="mailto:cedric.colin35@gmail.com">{{ $t('bonuses.mentions.intellectual.email') }}</a>
-			</p>
+      <p>{{ $t('bonuses.mentions.intellectual.contact') }}
+        <a class="underline text-blue-400"
+           href="mailto:cedric.colin35@gmail.com">{{ $t('bonuses.mentions.intellectual.email') }}</a>
+      </p>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.personal.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.personal.title') }}</h2>
 
-			<router-link class="underline text-blue-400" :to="{name: 'donnees_personnelles'}">
-				{{ $t('bonuses.mentions.personal.link') }}
-			</router-link>
+      <router-link class="underline text-blue-400" :to="{name: 'donnees_personnelles'}">
+        {{ $t('bonuses.mentions.personal.link') }}
+      </router-link>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.hyperlinks.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.hyperlinks.title') }}</h2>
 
-			<p>{{ $t('bonuses.mentions.hyperlinks.text') }}</p>
+      <p>{{ $t('bonuses.mentions.hyperlinks.text') }}</p>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.warning.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.warning.title') }}</h2>
 
-			<p>{{ $t('bonuses.mentions.warning.text1') }}</p>
-			<p>{{ $t('bonuses.mentions.warning.text2') }}</p>
+      <p>{{ $t('bonuses.mentions.warning.text1') }}</p>
+      <p>{{ $t('bonuses.mentions.warning.text2') }}</p>
 
-			<hr class="my-10">
+      <hr class="my-10">
 
-			<h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.law.title') }}</h2>
+      <h2 class="text-xl font-bold mb-5">{{ $t('bonuses.mentions.law.title') }}</h2>
 
-			<p>{{ $t('bonuses.mentions.law.text') }}</p>
-		</div>
+      <p>{{ $t('bonuses.mentions.law.text') }}</p>
+    </div>
 
 
-		<FooterComponent></FooterComponent>
-	</div>
+    <FooterComponent></FooterComponent>
+  </div>
 </template>
 
 <script>
 import FooterComponent from "@/components/navigation/footer/FooterComponent.vue";
 
 export default {
-	name: "MentionsLegales",
-	components: {FooterComponent}
+  name: "MentionsLegales",
+  components: {FooterComponent}
 }
 </script>
